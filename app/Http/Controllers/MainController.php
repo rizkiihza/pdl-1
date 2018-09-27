cd7cf0dcd423727f4b493afb6de9b3343e85c971
rizkiihza <rizki.ihza@gmail.com>
2018-09-27 14:01:51 +0700

    projection half done

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
@@ -14,7 +15,14 @@ class MainController extends Controller
         }
 
         else if ($query_pieces[0] === "projection") {
-            echo 2;
+            if (count($query_pieces) != 3) {
+                return redirect('/');
+            }
+
+            $columns = $query_pieces[1];
+            $table = $query_pieces[2];
+
+            $this->projection($columns, $table);
         }
 
         else if ($query_pieces[0] === "join") {
@@ -27,6 +35,22 @@ class MainController extends Controller
 
         else if ($query_pieces[0] == "delete") {
             echo 5;
+        }
+    }
+
+    private function projection($column, $table) {
+        if ($column == "*") {
+            $result = DB::table($table)->get();
+            foreach ($result as $row) {
+                echo json_encode($row) . '<br>';
+            }
+        }
+
+        else {
+            $result = DB::table($table)->select($column, 'valid_start', 'valid_end')->get();
+            foreach ($result as $row) {
+                echo json_encode($row) . '<br>';
+            }
         }
     }
 }
